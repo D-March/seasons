5a75da71756721c9a50f5c4364fa8b8f3c99050f
DMarchewka <30374304+D-March@users.noreply.github.com>
2020-09-14 18:52:53 +0100

    Updates

---
+++ precache-manifest.a97354fd4857a8db9bf78d0deeeabb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067a41f14fe025337707a066b15387fa",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "cd4f98b962a4cca49f9a",
+    "url": "/seasons/static/css/main.4310fbac.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4f98b962a4cca49f9a",
+    "url": "/seasons/static/js/main.55790e2d.chunk.js"
+  },
+  {
+    "revision": "7aa38b62a80534558f86",
+    "url": "/seasons/static/js/runtime-main.53d2d0be.js"
+  }
+]);
